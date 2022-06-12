c0c6bd55234a3198c5305e6ba765d6a56e7e9053
BSPDN <106454791+BSPDN@users.noreply.github.com>
2022-06-12 11:16:08 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,8 +1,9 @@
 let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, whatsapp, nameowner, m)
+conn.sendContact(m.chat, numberowner, nameowner, m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 handler.command = /^(owner|creator)$/i
+
 
 module.exports = handler
