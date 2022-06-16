7b908f54eaad9dd8994dc6d380ef94911e40da43
BSPDN <106454791+BSPDN@users.noreply.github.com>
2022-06-16 16:59:46 +0700

    Update hehe.js

--- plugins/hehe.js
+++ plugins/hehe.js
@@ -1,5 +1,5 @@
 let hikki = require("hikki-me")
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, args, split }) => {
 const text = args.join(' ')
 if (!text) return m.reply(`Example .topupff id|jumlah\nList Diamond Yang Di Dukung\nNominal Diamond\n5 Dm\n12 Dm\n70 Dm\n140 Dm\n355 Dm\n720 Dm`)
 try {
@@ -12,7 +12,7 @@ return await hikki.game.payDiamond(makeSession, '0895410636348')
 }
 const top = await topup() 
 conn.sendFile(m.chat, top.qrCode, 'Qris.jpg', 'Payment : ${top.paymentMethod}\nId : ${id}\nJumlah : ${jumlah} Diamond\nScan & Bayar Maksimal 30 detik setelah qr ini keluar', m)
-} catch (e) { return reply(`Sistem Error atau Nominal Diamond/Id\nUser Tidak Ada\nList Nominal Diamond\n5 Dm\n12 Dm\n70 Dm\n140 Dm\n355 Dm\n720 Dm`) }
+} catch (e) { return m.reply(`Sistem Error atau Nominal Diamond/Id\nUser Tidak Ada\nList Nominal Diamond\n5 Dm\n12 Dm\n70 Dm\n140 Dm\n355 Dm\n720 Dm`) }
 }
 handler.help = ['topupff id|jumlah']
 handler.tags = ['internet']
